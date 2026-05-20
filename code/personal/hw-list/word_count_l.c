4ded99659075cb14d229c2b589d1f9f049022d09
Teiji Schoyen <tjs392@drexel.edu>
2026-05-19 23:30:52 -0400

    hw1

--- code/personal/hw-list/word_count_l.c
+++ code/personal/hw-list/word_count_l.c
@@ -27,31 +27,55 @@
 #include "word_count.h"
 
 void init_words(word_count_list_t* wclist) { /* TODO */
+  list_init(wclist);
 }
 
 size_t len_words(word_count_list_t* wclist) {
-  /* TODO */
-  return 0;
+  return list_size(wclist);
 }
 
 word_count_t* find_word(word_count_list_t* wclist, char* word) {
-  /* TODO */
+  struct list_elem* curr = list_begin(wclist);
+  while (curr != list_end(wclist)) {
+    
+    word_count_t* wc = list_entry(curr, struct word_count, elem);
+    if (strcmp(wc->word, word) == 0) {
+      return wc;
+    }
+    curr = list_next(curr);
+  }
   return NULL;
 }
 
 word_count_t* add_word(word_count_list_t* wclist, char* word) {
-  /* TODO */
-  return NULL;
+  word_count_t* elem = find_word(wclist, word);
+  if (elem != NULL) {
+    elem->count++;
+    return elem;
+  }
+
+  word_count_t* new_wc = malloc(sizeof(word_count_t));
+  new_wc->word = word;
+  new_wc->count = 1; 
+  list_insert(list_begin(wclist), &new_wc->elem); 
+  return new_wc;
+  
 }
 
 void fprint_words(word_count_list_t* wclist, FILE* outfile) {
-  /* TODO */
-  /* Please follow this format: fprintf(<file>, "%i\t%s\n", <count>, <word>); */
+  struct list_elem* curr = list_begin(wclist);
+  while (curr != list_end(wclist)) {
+    word_count_t* curr_wc = list_entry(curr, struct word_count, elem);
+    fprintf(outfile, "%i\t%s\n", curr_wc->count, curr_wc->word);
+    curr = list_next(curr);
+  }   
 }
 
 static bool less_list(const struct list_elem* ewc1, const struct list_elem* ewc2, void* aux) {
-  /* TODO */
-  return false;
+  word_count_t* wc1 = list_entry(ewc1, struct word_count, elem);
+  word_count_t* wc2 = list_entry(ewc2, struct word_count, elem);
+  bool (*less)(const word_count_t*, const word_count_t*) = aux;
+  return less(wc1, wc2);
 }
 
 void wordcount_sort(word_count_list_t* wclist,
